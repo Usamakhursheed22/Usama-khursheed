00e292a8dfc88d982dbc5c5c7f600a6d7039ff5a
Usamakhursheed22 <156845955+Usamakhursheed22@users.noreply.github.com>
2024-01-17 21:52:20 +0500

    Add files via upload

---
+++ task.py
@@ -0,0 +1,41 @@
+#Usama Khursheed
+#Task 1
+#Write a Python program to do arithmetical  operations addition and division.
+#sum of 5+6
+
+sum = (5+6)
+print(sum)
+
+#dividion of 25/5
+
+divide = (25/5)
+print(divide)
+
+#Task 2
+#Write a Python program to find the area of a triangle.
+
+base_of_tringle = 10
+height_of_tringle = 15
+print("the area of tringle is",1/2(base_of_tringle)*(height_of_tringle))
+
+#Write a Python program to convert Celsius to Fahrenheit.
+#formula c*(9/5)*32
+temperature_in_C = 37
+print("temperature_in_C"*(9/5)*32)
+
+#Write a Python program that return all datatypes that we discussed in the class
+#data type
+
+a = int(input("enter num"))
+print("the data type of given num is",type(a))
+
+b = float(input("enter num"))
+print("the data type of given num is",type(b))
+
+c = str(input("enter value"))
+print("the data type of given value is",type(c))
+
+d = bool(input("enter value"))
+print("the data type of given value is",type(d))
+
+
